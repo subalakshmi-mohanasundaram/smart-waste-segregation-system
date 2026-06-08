8c4aaaa07eaf4e983feb4d575f2e43cd5c963b44
subalakshmimohanasundaram <subalakshmimohanasundaram@gmail.com>
2026-06-08 22:15:06 +0530

    Fixed API URL for AWS Deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://54.174.138.233:5000/api",
 });
 
 api.interceptors.request.use((req) => {
